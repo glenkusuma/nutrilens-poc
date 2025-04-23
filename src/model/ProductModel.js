c4c5378a5bb6b7f375d9f43fb32c03e20b6c49e9
glenkusuma <91659118+glenkusuma@users.noreply.github.com>
2025-04-23 18:42:43 +0700

    refactor: improve fetch URL construction in ProductModel; improve category methods for better readability and optimization

--- src/model/ProductModel.js
+++ src/model/ProductModel.js
@@ -2,7 +2,7 @@ export class ProductModel {
   baseUrl = 'https://id.openfoodfacts.org/api/v2/';
 
   async getProductByBarcode(barcode) {
-    const res = await fetch(`${this.baseUrl}/product/${barcode}`);
+    const res = await fetch(`${this.baseUrl}product/${barcode}`);
     if (!res.ok) throw new Error('Gagal mengambil produk');
     return await res.json();
   }
@@ -15,7 +15,9 @@ export class ProductModel {
   }
 
   async getByCategory(keyword, size = 10) {
-    const url = `${this.baseUrl}/search?categories_tags=${encodeURIComponent(keyword)}&countries_tags_en=id&sort_by=scans_n&page_size=${size}`;
+    const fields =
+      'code,product_name,nutrition_grades,categories_tags_en,image_front_small_url';
+    const url = `${this.baseUrl}search?categories_tags=${encodeURIComponent(keyword)}&countries_tags_en=id&sort_by=scans_n&page_size=${size}&fields=${fields}`;
     const res = await fetch(url);
     if (!res.ok) throw new Error('Gagal mengambil kategori');
     return await res.json();
